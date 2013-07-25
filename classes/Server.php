f553471f8ba1c26924080cf26abfd2da69c5cdfe
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 12:03:05 -0400

    Overload __set method and add server_ip() method

--- classes/Server.php
+++ classes/Server.php
@@ -28,6 +28,32 @@ class Server {
 
     }
 
+
+    function __set($name, $value) {
+        switch ($name) {
+            case 'name':
+                $this->db->query("UPDATE servers SET name = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->name = $value;
+                break;
+            case 'address':
+                $this->db->query("UPDATE servers SET address = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->address = $value;
+                break;
+            case 'owner':
+                $this->db->query("UPDATE servers SET owner = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->owner = $value;
+                break;
+            case 'info':
+                $this->db->query("UPDATE servers SET info = ? WHERE id = ?", "si", array(serialize($value), $this->id));
+                $this->info = $value;
+                break;
+            case 'updated':
+                $this->db->query("UPDATE servers SET updated = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->updated = $value;
+                break;
+        }
+    }
+
     function force_update() {
     	$this->info = bzfquery($this->address);
     	$this->db->query("UPDATE servers SET info = ? WHERE id = ?", "si", array(serialize($this->info), $this->id));
@@ -54,11 +80,14 @@ class Server {
      * Returns if the last update was done greater than UPDATE_INTERVAL mins ago
      */
     function stale_info() {
-
         $now = new DateTime("now");
         $last_update = $this->updated->diff($now);
 
         return $last_update->format('%i') > UPDATE_INTERVAL;
+    }
+
+    function server_ip() {
+        return $this->info['ip'];
     }
 
 }
