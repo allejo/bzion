a10702ea0623ac171e258e10b7943db84c7c866b
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 01:43:11 -0400

    Add enter_match and calculate_elo_diff functions

--- classes/Match.php
+++ classes/Match.php
@@ -40,4 +40,40 @@ class Match {
 
     }
 
+    public static function enter_match($a, $b, $a_points, $b_points, $duration, $entered_by, $timestamp = "now") {
+
+        $result = $this->db->query("SELECT elo FROM teams WHERE id = ?", "i", array($a));
+        $a_elo = $result['elo'];
+        $result = $this->db->query("SELECT elo FROM teams WHERE id = ?", "i", array($b));
+        $b_elo = $result['elo'];
+
+        $diff = calculate_elo_diff($a_elo, $b_elo, $team_a_points, $team_a_points, $duration);
+
+        $a_elo += $diff;
+        $b_elo -= $diff;
+        $diff = abs($diff);
+
+        $timestamp = new DateTime($timestamp);
+        
+        $results = $this->db->query("INSERT INTO matches (team_a, team_b, team_a_points, team_b_points, team_a_elo, team_b_elo, elo_diff, timestamp, updated, duration, entered_by, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        "iiiiiiissiii", array($a, $b, $a_points, $b_points, $a_elo, $b_elo, $diff, $timestamp, $timestamp, $duration, $entered_by, 0));
+    }
+
+    function calculate_elo_diff($a_elo, $b_elo, $a_points, $b_points, $duration) {
+        $prob = 1.0 / (1 + 10 ^ (($team_b-$team_a)/400.0));
+        if ($a_points > $b_points) {
+           $diff = 50*(1-$prob);
+        } else if ($a_points == $b_points) {
+            $diff = 50*(0.5-$prob);
+        } else {
+            $diff = 50*(0-$prob);
+        }
+
+        if ($duration == 20) {
+            return (2/3)*$diff;
+        }
+
+        return $diff;
+    }
+
 }
