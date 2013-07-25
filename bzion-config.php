fcdcb2783bd1390c9045754ebe7b6da541049b44
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 10:51:34 -0400

    Add LIST_SERVER constant

--- bzion-config.php
+++ bzion-config.php
@@ -3,10 +3,10 @@
 /*
  * Set the credentials used for the MySQL database
  */
-DEFINE('MYSQL_HOST', 'localhost');
-DEFINE('MYSQL_DB_NAME', 'bzion');
-DEFINE('MYSQL_USER', 'bzion_admin');
-DEFINE('MYSQL_PASSWORD', 'password');
+DEFINE("MYSQL_HOST", "localhost");
+DEFINE("MYSQL_DB_NAME", "bzion");
+DEFINE("MYSQL_USER", "bzion_admin");
+DEFINE("MYSQL_PASSWORD", "password");
 
 /*
  * An array of directories to be autoloaded in bzion-config.php
@@ -14,3 +14,8 @@ DEFINE('MYSQL_PASSWORD', 'password');
 $classesDir = array (
     'classes/'
 );
+
+/*
+ * Misc settings
+ */
+DEFINE("LIST_SERVER", "http://my.bzflag.org/db/?action=LIST&version=BZFS0221");
