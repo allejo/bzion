5e8ddd826fcfa3c4e162064d45d2204458ed6e7d
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 11:47:51 -0400

    Add several Server methods

--- classes/Server.php
+++ classes/Server.php
@@ -30,8 +30,35 @@ class Server {
 
     function force_update() {
     	$this->info = bzfquery($this->address);
-
     	$this->db->query("UPDATE servers SET info = ? WHERE id = ?", "si", array(serialize($info), $this->id));
+    }
+
+    function is_online() {
+        $servers = file(LIST_SERVER);
+        return in_array($this->address, $servers);
+    }
+
+    function has_players() {
+        return $this->info['numPlayers'] > 0;
+    }
+
+    function num_players() {
+        return $this->info['numPlayers'];
+    }
+
+    function get_players() {
+        return $this->info['player'];
+    }
+
+    /*
+     * Returns if the last update was done greater than UPDATE_INTERVAL mins ago
+     */
+    function stale_info() {
+
+        $now = new DateTime("now");
+        $last_update = $this->updated->diff($now);
+
+        return $last_update->format('%i') > UPDATE_INTERVAL;
     }
 
 }
