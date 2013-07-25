b532845e501e15fdab8680ed983eb727bc711b9c
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 13:16:37 -0400

    Add phpdoc

--- classes/Player.php
+++ classes/Player.php
@@ -2,21 +2,88 @@
 
 class Player {
 
+    /**
+     * The id of the player
+     * @var int
+     */
     private $id;
+
+    /**
+     * The bzid of the player
+     * @var int
+     */
     private $bzid;
+
+    /**
+     * The id of the player's team
+     * @var int
+     */
     private $team;
+
+    /**
+     * The username of the player
+     * @var string
+     */
     private $username;
+
+    /**
+     * The player's status
+     * @var string
+     */
     private $status;
+
+    /**
+     * The access level of the player
+     * @var int
+     */
     private $access;
+
+    /**
+     * The url of the player's profile avatar
+     * @var string
+     */
     private $avatar;
+
+    /**
+     * The player's profile description
+     * @var string
+     */
     private $description;
+
+    /**
+     * The id of the player's country
+     * @var int
+     */
     private $country;
+
+    /**
+     * The player's timezone, in terms of distance from UTC (e.x. -5 for UTC-5)
+     * @var int
+     */
     private $timezone;
+
+    /**
+     * The date the player joined the site
+     * @var string
+     */
     private $joined;
+
+    /**
+     * The date of the player's last login
+     * @var string
+     */
     private $last_login;
 
+    /**
+     * The database variable used for queries
+     * @var MySQLi
+     */
     private $db;
 
+    /**
+     * Construct a new Player
+     * @param int $bzid The player's bzid
+     */
     function __construct($bzid) {
 
         $this->db = new Database();
