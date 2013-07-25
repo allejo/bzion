d4cf53e11eccd9a9113385b933864cce116f934d
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 10:26:59 -0400

    Spacing

--- bzion-config.php
+++ bzion-config.php
@@ -3,10 +3,10 @@
 /*
  * Set the credentials used for the MySQL database
  */
-DEFINE ('MYSQL_HOST', 'localhost');
-DEFINE ('MYSQL_DB_NAME', 'bzion');
-DEFINE ('MYSQL_USER', 'bzion_admin');
-DEFINE ('MYSQL_PASSWORD', 'password');
+DEFINE('MYSQL_HOST', 'localhost');
+DEFINE('MYSQL_DB_NAME', 'bzion');
+DEFINE('MYSQL_USER', 'bzion_admin');
+DEFINE('MYSQL_PASSWORD', 'password');
 
 /*
  * An array of directories to be autoloaded in bzion-config.php
