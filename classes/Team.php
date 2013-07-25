8c3c85b5e9e21f18226e141763b2c728eda85c38
Matthew Pavia <matt.pavia@gmail.com>
2013-07-24 23:34:04 -0400

    Add Team class

---
+++ classes/Team.php
@@ -0,0 +1,54 @@
+<?php
+
+class Team {
+	
+	private $id;
+	private $name;
+	private $description;
+	private $avatar;
+	private $created;
+	private $elo;
+	private $activity;
+	private $leader;
+	private $mathes_won;
+	private $mathes_lost;
+	private $mathes_draw;
+	private $members;
+	private $status;
+
+	private $db;
+
+	function __construct($id) {
+
+		$this->db = new Database();
+
+		$results = $this->db->query("SELECT * FROM teams WHERE id = '" . $id . "'");
+
+		$this->id = $results['id'];
+		$this->name = $results['name'];
+		$this->description = $results['description'];
+		$this->avatar = $results['avatar'];
+		$this->created = new DateTime($results['created']);
+		$this->elo = $reults['elo'];
+		$this->activity = $results['activity'];
+		$this->leader = $result['leader'];
+		$this->matches_won = $results['matches_won'];
+		$this->matches_lost = $results['matches_lost'];
+		$this->matches_draw = $results['matches_draw'];
+		$this->members = $results['members'];
+		$this->status = $results['status'];
+
+	}
+
+	function total_matches() {
+		return ($this->matches_won + $this->matches_lost + $this->matches_draw);
+	}
+
+	function members() {
+		$members = $this->db->query("SELECT * FROM users WHERE team = '" . $this->id . "'");
+		return $members;
+	}
+
+}
+
+?>
