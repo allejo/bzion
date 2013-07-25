c43a9d64dc2f65c8e86bcb19c38edaa02b6c429a
Matthew Pavia <matt.pavia@gmail.com>
2013-07-24 23:58:41 -0400

    Add Player class

---
+++ classes/Player.php
@@ -0,0 +1,40 @@
+<?php
+
+class Player {
+	
+	private $id;
+	private $bzid;
+	private $team;
+	private $username;
+	private $status;
+	private $access;
+	private $avatar;
+	private $description;
+	private $country;
+	private $timezone;
+	private $joined;
+	private $last_login;
+
+	private $db;
+
+	function __construct($bzid) {
+
+		$this->db = new Database();
+		$this->bzid = $bzid;
+
+		$results = $this->db->query("SELECT * FROM players WHERE bzid = '" . $bzid . "'");
+
+		$this->id = $results['id'];
+		$this->username = $results['username'];
+		$this->status = $results['status'];
+		$this->access = $results['access'];
+		$this->avatar = $results['avatar'];
+		$this->description = $results['description'];
+		$this->country = $results['country'];
+		$this->timezone = $results['timezone'];
+		$this->joined = new DateTime($results['joined']);
+		$this->last_login = new DateTime($results['last_login']);
+
+	}
+
+}
