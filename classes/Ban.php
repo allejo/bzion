bc514853b71cf6d5431dd499df9d04cc0206a830
Matthew Pavia <matt.pavia@gmail.com>
2013-07-25 13:27:58 -0400

    Add Ban class

---
+++ classes/Ban.php
@@ -0,0 +1,99 @@
+<?php
+
+class Ban {
+
+    /**
+     * The id of the ban
+     * @var int
+     */
+    private $id;
+
+    /**
+     * The bzid of the banned player
+     * @var id
+     */
+    private $player;
+
+    /**
+     * The ban expiration date
+     * @var string
+     */
+    private $expiration;
+
+    /**
+     * The ban reason
+     * @var string
+     */
+    private $reason;
+
+    /**
+     * The ban creation date
+     * @var string
+     */
+    private $created;
+
+    /**
+     * The date the ban was last updated
+     * @var string
+     */
+    private $updated;
+
+    /**
+     * The bzid of the ban author
+     * @var bzid
+     */
+    private $author;
+
+    /**
+     * The database variable used for queries
+     * @var MySQLi
+     */
+    private $db;
+
+    /**
+     * Construct a new Ban
+     * @param int $id The ban's id
+     */
+    function __construct($id) {
+
+        $this->db = new Database();
+        $this->id = $id;
+
+        $results = $this->db->query("SELECT * FROM bans WHERE id = ?", "i", array($id));
+
+        $this->player = $results['player'];
+        $this->expiration = new DateTime($results['expiration']);
+        $this->reason = $results['reason'];
+        $this->created = unserialize($results['created']);
+        $this->updated = new DateTime($results['updated']);
+        $this->author = $results['author'];
+
+    }
+
+    /**
+     * Overload __set to update instance variables and database
+     * @param string $name The variable's name
+     * @param mixed $value The variable's new value
+     */
+    function __set($name, $value) {
+        switch ($name) {
+            case 'expiration':
+                $this->db->query("UPDATE bans SET expiration = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->expiration = $value;
+                break;
+            case 'reason':
+                $this->db->query("UPDATE bans SET reason = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->address = $value;
+                break;
+            case 'updated':
+                $this->db->query("UPDATE bans SET updated = ? WHERE id = ?", "si", array($value, $this->id));
+                $this->updated = $value;
+                break;
+            case 'author':
+                $this->db->query("UPDATE bans SET info = ? WHERE id = ?", "si", array(serialize($value), $this->id));
+                $this->author = $value;
+                break;
+        }
+    }
+
+}
