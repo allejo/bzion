f4abf639b4437fc0a3025a2556e405a4a1047253
Vladimir Jimenez <allejo@me.com>
2013-07-29 12:59:44 -0700

    Fixed typo of static

--- classes/Header.php
+++ classes/Header.php
@@ -62,7 +62,7 @@ class Header {
      * Redirect the page using PHP's header() function
      * @param string $location The page to redirect to
      */
-    public stati function go($location = "index.php") {
+    public static function go($location = "index.php") {
         if (strtolower($location) == "default" || strtolower($location) == "index.php") {
             header("Location: index.php");
         } else {
