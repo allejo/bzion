5207baf8ce0fbec4d6669ace20ac80f3aea70e6e
kongr45gpen <kongr45gpen@helit.org>
2013-07-30 18:04:23 +0300

    Move Team::generateAlias() into Controller::generateAlias()

--- classes/Controller.php
+++ classes/Controller.php
@@ -68,4 +68,63 @@ abstract class Controller {
     }
 
 
+    /**
+     * Generate a URL-friendly unique alias for an object name
+     *
+     * @param string $name The original object name
+     * @return string|Null The generated alias, or Null if we couldn't make one
+     */
+    static function generateAlias($name) {
+        // Convert name to lowercase
+        $name = strtolower($name);
+
+        // List of characters which should be converted to dashes
+        $makeDash = array(' ', '_');
+
+        $name = str_replace($makeDash, '-', $name);
+
+        // Only keep letters, numbers and dashes - delete everything else
+        $name = preg_replace("/[^a-zA-Z\-0-9]+/", "", $name);
+
+        if (str_replace('-', '', $name) == '') {
+            // The name only contains symbols or Unicode characters!
+            // This means we can't convert it to an alias
+            return null;
+        }
+
+        // An alias name can't only contain numbers, because it will be
+        // indistinguishable from an ID. If it does, add a dash in the end.
+        if (preg_match("/^[0-9]+$/", $name)) {
+            $name = $name . '-';
+        }
+
+        // Try to find duplicates
+        $db = Database::getInstance();
+        $result = $db->query("SELECT alias FROM " . $this->table . " WHERE alias REGEXP ?", 's', array("^".$name."[0-9]*$"));
+
+        // The functionality of the following code block is provided in PHP 5.5's
+        // array_column function. What is does is convert the multi-dimensional
+        // array that $db->query() gave us into a single-dimensional one.
+        $aliases = array();
+        if (is_array($result)) {
+            foreach ($result as $r) {
+                $aliases[] = $r['alias'];
+            }
+        }
+
+        // No duplicates found
+        if (!in_array($name, $aliases))
+            return $name;
+
+        // If there's already an entry with the alias we generated, put a number
+        // in the end of it and keep incrementing it until there is we find
+        // an open spot.
+        $i = 2;
+        while(in_array($name.$i, $aliases)) {
+            $i++;
+        }
+
+        return $name.$i;
+    }
+
 }
