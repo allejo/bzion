d46e5a101eb85efcd8d900caff81ef01d9e69959
kongr45gpen <kongr45gpen@helit.org>
2013-07-30 11:34:47 +0300

    Add suport for results on the Controller class Apply some fixes and add PHPDoc

--- classes/Controller.php
+++ classes/Controller.php
@@ -15,6 +15,12 @@ abstract class Controller {
     protected $table;
 
     /**
+     * The result of the database query to locate the object with a specific ID
+     * @var array
+     */
+    protected $result;
+
+    /**
      * The database variable used for queries
      * @var Database
      */
@@ -22,15 +28,24 @@ abstract class Controller {
 
     /**
      * Construct a new Controller
+     *
+     * This method takes the table and ID of the object to look for, and
+     * creates a $this->db object which can be used to communicate with the
+     * database, as well as a $this->result array which is the single result
+     * of the $this->db->query function
+     *
+     * @param int $id The ID of the object to look for
      * @param int $table The name of the DB table used for queries
      */
     function __construct($id, $table=null) {
 
         $this->db = Database::getInstance();
 
-        $this->id = id;
+        $this->id = $id;
         $this->table = $table;
 
+        $results = $this->db->query("SELECT * FROM " . $table . " WHERE id = ?", "i", array($id));
+        $this->result = $results[0];
     }
 
 }
