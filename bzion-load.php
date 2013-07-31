b0ea51cfd1c7bba838b50fc21de44492d98a1b08
Matthew Pavia <matt.pavia@gmail.com>
2013-07-31 13:26:34 -0400

    Add missing closing paren

--- bzion-load.php
+++ bzion-load.php
@@ -12,7 +12,7 @@ function __autoload($class_name)
     foreach ($classesDir as $directory)
     {
         $doc = rtrim(DOC_ROOT, '/') . '/' . $directory . $class_name . '.php');
-        if (file_exists($doc)
+        if (file_exists($doc))
         {
             require_once($doc);
             return;
