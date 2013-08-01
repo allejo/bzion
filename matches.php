3cfbf6c5f1fad96dc4d040a5a96c4727bdc0766c
Matthew Pavia <matt.pavia@gmail.com>
2013-08-01 17:38:51 -0400

    Add rewrite rule for matches and add some documentation

---
+++ matches.php
@@ -0,0 +1,54 @@
+<?php
+
+include("bzion-load.php");
+
+$header = new Header();
+
+
+if (isset($_GET['alias'])) {
+    $team = Team::getFromAlias($_GET['alias']);
+} else if (isset($_GET['id'])) {
+    $team = new Team($_GET['id']);
+}
+
+if (isset($team)) {
+    $header->draw("Teams :: " . $team->getName());
+
+    echo "<br /><br />";
+    echo "<b>" . $team->getName() . "</b><br />";
+    echo "ELO: " . $team->getElo() . "<br />";
+    echo "Matches: " . $team->getNumTotalMatches() . "<br />";
+    echo "Members: " . $team->getNumMembers() . "<br />";
+    $leader = $team->getLeader();
+    echo "Leader: " . $leader['username'] . "<br />";
+    echo "Activity: " . $team->getActivity() . "<br />";
+    echo "Created: " . $team->getCreationDate() . "<br />";
+    echo "<br />";
+
+} else {
+
+    $header->draw("Teams");
+
+    $teams = Team::getTeams();
+
+    echo "<br /><br />";
+
+    foreach ($teams as $key => $value) {
+        $team = new Team($value['id']);
+        echo "<b><a href='" . $team->getURL() . "'>" . $team->getName() . "</a></b><br />";
+        echo "ELO: " . $team->getElo() . "<br />";
+        echo "Matches: " . $team->getNumTotalMatches() . "<br />";
+        echo "Members: " . $team->getNumMembers() . "<br />";
+        $leader = $team->getLeader();
+        echo "Leader: " . $leader['username'] . "<br />";
+        echo "Activity: " . $team->getActivity() . "<br />";
+        echo "Created: " . $team->getCreationDate() . "<br />";
+        echo "<br />";
+    }
+
+}
+
+$footer = new Footer();
+$footer->draw();
+
+?>
