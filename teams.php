22986dafe844ebf8c2eb4c470655fe75a409992e
kongr45gpen <electrovesta@gmail.com>
2013-08-01 09:27:57 +0300

    Less repetitive code in teams.php

--- teams.php
+++ teams.php
@@ -6,26 +6,12 @@ $header = new Header();
 
 
 if (isset($_GET['alias'])) {
-
     $team = Team::getFromAlias($_GET['alias']);
-
-    $header->draw("Teams :: " . $team->getName());
-
-    echo "<br /><br />";
-    echo "<b>" . $team->getName() . "</b><br />";
-    echo "ELO: " . $team->getElo() . "<br />";
-    echo "Matches: " . $team->getNumTotalMatches() . "<br />";
-    echo "Members: " . $team->getNumMembers() . "<br />";
-    $leader = $team->getLeader();
-    echo "Leader: " . $leader['username'] . "<br />";
-    echo "Activity: " . $team->getActivity() . "<br />";
-    echo "Created: " . $team->getCreationDate() . "<br />";
-    echo "<br />";
-
 } else if (isset($_GET['id'])) {
-
     $team = new Team($_GET['id']);
+}
 
+if (isset($team)) {
     $header->draw("Teams :: " . $team->getName());
 
     echo "<br /><br />";
