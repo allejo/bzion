e383f60b8dddba355cca49601beb09d04f793dd8
Matthew Pavia <matt.pavia@gmail.com>
2013-08-01 18:24:27 -0400

    Use Match methods to get info

--- matches.php
+++ matches.php
@@ -11,14 +11,14 @@ $matches = Match::getMatches();
 echo "<br /><br />";
 
 foreach ($matches as $key => $value) {
-    //$match = new Match($value['id']);
-    $team_a = new Team($value['team_a']);
-    $team_b = new Team($value['team_b']);
-    echo "<b>" . $team_a->getName() . " (" . $value['team_a_points'] . " points) vs " . $team_b->getName() . " (" . $value['team_b_points'] . " points) </b><br />";
-    echo "+/- " . $value['elo_diff'] . "<br />";
-    echo $team_a->getName() . "'s new ELO: " . $value['team_a_elo_new'] . "<br />";
-    echo $team_b->getName() . "'s new ELO: " . $value['team_b_elo_new'] . "<br />";
-    echo "Duration: " . $value['duration'] . " min <br />";
+    $match = new Match($value['id']);
+    $team_a = new Team($match->getTeamA());
+    $team_b = new Team($match->getTeamB());
+    echo "<b>" . $team_a->getName() . " (" . $match->getTeamAPoints() . " points) vs " . $team_b->getName() . " (" . $match->getTeamBPoints() . " points) </b><br />";
+    echo "+/- " . $match->getEloDiff() . "<br />";
+    echo $team_a->getName() . "'s new ELO: " . $match->getTeamAEloNew() . "<br />";
+    echo $team_b->getName() . "'s new ELO: " . $match->getTeamBEloNew() . "<br />";
+    echo "Duration: " . $match->getDuration() . " min <br />";
     echo "Timestamp: " . $match->getTimestamp() . "<br />";
     echo "<br />";
 }
