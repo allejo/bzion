cc22a39bd415bbd84438b722836e397e59781455
Matthew Pavia <matt.pavia@gmail.com>
2013-08-02 12:11:57 -0400

    Add ability to view a team's list of matches

--- matches.php
+++ matches.php
@@ -4,23 +4,54 @@ include("bzion-load.php");
 
 $header = new Header();
 
-$header->draw("Matches");
 
-$matches = Match::getMatches();
+if (isset($_GET['alias'])) {
+    $team = Team::getFromAlias($_GET['alias']);
+} else if (isset($_GET['id'])) {
+    $team = new Team($_GET['id']);
+}
 
-echo "<br /><br />";
+if (isset($team)) {
+    $header->draw("Matches :: " . $team->getName());
 
-foreach ($matches as $key => $value) {
-    $match = new Match($value['id']);
-    $team_a = new Team($match->getTeamA());
-    $team_b = new Team($match->getTeamB());
-    echo "<b>" . $team_a->getName() . " (" . $match->getTeamAPoints() . " points) vs " . $team_b->getName() . " (" . $match->getTeamBPoints() . " points) </b><br />";
-    echo "+/- " . $match->getEloDiff() . "<br />";
-    echo $team_a->getName() . "'s new ELO: " . $match->getTeamAEloNew() . "<br />";
-    echo $team_b->getName() . "'s new ELO: " . $match->getTeamBEloNew() . "<br />";
-    echo "Duration: " . $match->getDuration() . " min <br />";
-    echo "Timestamp: " . $match->getTimestamp() . "<br />";
-    echo "<br />";
+    $matches = $team->getMatches();
+
+    echo "<br /><br />";
+
+    foreach ($matches as $key => $value) {
+        $match = new Match($value['id']);
+        $team_a = new Team($match->getTeamA());
+        $team_b = new Team($match->getTeamB());
+        echo "<b>" . $team_a->getName() . " (" . $match->getTeamAPoints() . " points) vs " . $team_b->getName() . " (" . $match->getTeamBPoints() . " points) </b><br />";
+        echo "+/- " . $match->getEloDiff() . "<br />";
+        echo $team_a->getName() . "'s new ELO: " . $match->getTeamAEloNew() . "<br />";
+        echo $team_b->getName() . "'s new ELO: " . $match->getTeamBEloNew() . "<br />";
+        echo "Duration: " . $match->getDuration() . " min <br />";
+        echo "Timestamp: " . $match->getTimestamp() . "<br />";
+        echo "<br />";
+    }
+
+} else {
+
+    $header->draw("Matches");
+
+    $matches = Match::getMatches();
+
+    echo "<br /><br />";
+
+    foreach ($matches as $key => $value) {
+        $match = new Match($value['id']);
+        $team_a = new Team($match->getTeamA());
+        $team_b = new Team($match->getTeamB());
+        echo "<b>" . $team_a->getName() . " (" . $match->getTeamAPoints() . " points) vs " . $team_b->getName() . " (" . $match->getTeamBPoints() . " points) </b><br />";
+        echo "+/- " . $match->getEloDiff() . "<br />";
+        echo $team_a->getName() . "'s new ELO: " . $match->getTeamAEloNew() . "<br />";
+        echo $team_b->getName() . "'s new ELO: " . $match->getTeamBEloNew() . "<br />";
+        echo "Duration: " . $match->getDuration() . " min <br />";
+        echo "Timestamp: " . $match->getTimestamp() . "<br />";
+        echo "<br />";
+    }
+
 }
 
 $footer = new Footer();
