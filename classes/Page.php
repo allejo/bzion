ad3858be3fd5d9850986d0e7c85c41d3d7d5e7e5
Matthew Pavia <matt.pavia@gmail.com>
2013-08-02 12:22:04 -0400

    Add Page class

---
+++ classes/Page.php
@@ -0,0 +1,64 @@
+<?php
+
+class Page extends Controller {
+
+    /**
+     * The name of the page
+     * @var string
+     */
+    private $name;
+
+    /**
+     * The content of the page
+     * @var string
+     */
+    private $content;
+
+    /**
+     * The creation date of the page
+     * @var string
+     */
+    private $created;
+
+    /**
+     * The date the page was last updated
+     * @var string
+     */
+    private $updated;
+
+    /**
+     * The bzid of the author of the page
+     * @var int
+     */
+    private $author;
+
+    /**
+     * The status of the page
+     * @var string
+     */
+    private $status;
+
+    /**
+     * The name of the database table used for queries
+     */
+    const TABLE = "pages";
+
+    /**
+     * Construct a new Page
+     * @param int $id The page's id
+     */
+    function __construct($id) {
+
+        parent::__construct($id);
+        $page = $this->result;
+
+        $this->name = $page['subject'];
+        $this->content = $page['content'];
+        $this->created = new DateTime($page['created']);
+        $this->updated = new DateTime($page['updated']);
+        $this->author = $news['author'];
+        $this->status = $news['page'];
+
+    }
+
+}
