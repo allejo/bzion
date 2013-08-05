dfd79535b70a3591406d65435dbfacc12c2e8a8f
Matthew Pavia <matt.pavia@gmail.com>
2013-08-05 17:32:00 -0400

    Add bans.php file

---
+++ bans.php
@@ -0,0 +1,26 @@
+<?php
+
+include("bzion-load.php");
+
+$header = new Header("Bans");
+
+$header->draw();
+
+$banList = Ban::getBans();
+
+echo "<br /><br />";
+
+foreach ($banList as $key => $id) {
+    $ban = new Ban($id);
+    $bannedPlayer = new Player($ban->getPlayer());
+    echo "<h4>" . $bannedPlayer->getUsername() . "</h4>";
+    $author = new Player($ban->getAuthor());
+    echo "<small>By " . $author->getUsername() . " at " . $ban->getUpdated() . "</small><br />";
+    echo $ban->getReason() . "<br />";
+    echo "<br />";
+}
+
+$footer = new Footer();
+$footer->draw();
+
+?>
