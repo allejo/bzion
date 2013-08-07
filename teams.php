412aa2d5f5ec8d62785b4af5070846b1156e34d4
Ashvala Vinay <mail@ashvala.net>
2013-08-07 18:57:38 -0400

    continue styling

--- teams.php
+++ teams.php
@@ -13,13 +13,13 @@ if (isset($_GET['alias'])) {
 
 if (isset($team)) {
     $header->draw("Teams :: " . $team->getName());
-
+    
     echo "<div class='team_name'>" . $team->getName() . "</div> <br />";
-    echo "ELO: " . $team->getElo() . "<br />";
+    echo "<div style='font-size:0.8em'>" ELO: " . $team->getElo() . "</div><br />";
+    $leader = $team->getLeader();
+    echo "<div style='font-size:0.8em'>Leader: " . $leader['username'] . "</div><br />";
     echo "Matches: <a href='" . $team->getMatchesURL() . "'>" . $team->getNumTotalMatches() . "</a><br />";
     echo "Members: " . $team->getNumMembers() . "<br />";
-    $leader = $team->getLeader();
-    echo "Leader: " . $leader['username'] . "<br />";
     echo "Activity: " . $team->getActivity() . "<br />";
     echo "Created: " . $team->getCreationDate() . "<br />";
     echo "<br />";
