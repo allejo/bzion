ba244cd9360e5f28c99d7b5302a6a3a977623856
Matthew Pavia <matt.pavia@gmail.com>
2013-08-07 11:17:35 -0400

    Add some getter methods for the Page class and fix some instance variables

--- classes/Page.php
+++ classes/Page.php
@@ -52,13 +52,59 @@ class Page extends Controller {
         parent::__construct($id);
         $page = $this->result;
 
-        $this->name = $page['subject'];
+        $this->name = $page['name'];
+        $this->alias = $page['alias'];
         $this->content = $page['content'];
         $this->created = new DateTime($page['created']);
         $this->updated = new DateTime($page['updated']);
         $this->author = $page['author'];
-        $this->status = $page['page'];
+        $this->status = $page['status'];
 
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getContent() {
+        return $this->content;
+    }
+
+    function getCreated() {
+        return $this->created->format(DATE_FORMAT);
+    }
+
+    function getUpdated() {
+        return $this->updated->format(DATE_FORMAT);
+    }
+
+    function getAuthor() {
+        return $this->author;
+    }
+
+    function getStatus() {
+        return $this->status;
+    }
+
+    /**
+     * Get the URL that points to the page
+     * @return string The page's URL, without a trailing slash
+     */
+    function getURL($dir="", $default=NULL) {
+        return parent::getURL($dir, $default);
+    }
+
+    public static function getPages() {
+        return parent::getIdsFrom("status", array("live"), "s");
+    }
+
+    /**
+     * Gets a page object from the supplied alias
+     * @param string $alias The page's alias
+     * @return Page The page
+     */
+    public static function getFromAlias($alias) {
+        return new Page(parent::getIdFrom($alias, "alias"));
     }
 
 }
