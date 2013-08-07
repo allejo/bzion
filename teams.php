d4efa89bdf2e7ae49d65e1c84f95510442e28887
Ashvala Vinay <mail@ashvala.net>
2013-08-07 19:32:58 -0400

    attempt at getting tables up

--- teams.php
+++ teams.php
@@ -29,16 +29,26 @@ if (isset($team)) {
     $header->draw("Teams");
 
     $teams = Team::getTeams();
-
+?>
+<table class="teams_table">
+<tr class="teams_tr">
+<th style="width:30%"> Name </th>
+<th> Rating </th>
+<th> Leader </th>
+<th> Members </th>
+<th> Matches </th>
+<th> Activity </th>
+</tr>
+<?php
     foreach ($teams as $key => $id) {
         $team = new Team($id);
-        echo "<b><a href='" . $team->getURL() . "'>" . $team->getName() . "</a></b><br />";
-        echo "ELO: " . $team->getElo() . "<br />";
-        echo "Matches: <a href='" . $team->getMatchesURL() . "'>" . $team->getNumTotalMatches() . "</a><br />";
-        echo "Members: " . $team->getNumMembers() . "<br />";
+        echo "<td><a href='" . $team->getURL() . "'>" . $team->getName() . "</td>";
+        echo "<td>" . $team->getElo() . "</td>";
         $leader = $team->getLeader();
-        echo "Leader: " . $leader['username'] . "<br />";
-        echo "Activity: " . $team->getActivity() . "<br />";
+        echo "<td>" . $leader['username'] . "</td>";
+        echo "<td> " . $team->getNumMembers() . "</td>";
+        echo "<td><a href='" . $team->getMatchesURL() . "'>" . $team->getNumTotalMatches() . "</a></td>";
+        echo "<td>" . $team->getActivity() . "</td>>";
         echo "Created: " . $team->getCreationDate() . "<br />";
         echo "<br />";
     }
