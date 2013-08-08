f81c6ee717a227f3c0c3f1503bdbce4347e3ce08
Ashvala Vinay <mail@ashvala.net>
2013-08-07 22:10:20 -0400

    buh

--- news.php
+++ news.php
@@ -12,13 +12,13 @@ $newsArticles = News::getNews();
 <?php
 foreach ($newsArticles as $key => $id) {
     $news = new News($id);
-    echo "<div class=\"news_box"\>"
-    echo "<div class='title_box'>"
-    echo "<div id='news_title'>" . $news->getSubject() . "</div>";
+    echo "<div class=\"news_box\">"
+    echo "<div class=\"title_box\">"
+    echo "<div id=\"news_title\">" . $news->getSubject() . "</div>";
     $author = new Player($news->getAuthor());
-    echo "<div id='author'>By " . $author->getUsername() . " at " . $news->getUpdated() . "</div>";
+    echo "<div id=\"author\">By " . $author->getUsername() . " at " . $news->getUpdated() . "</div>";
     echo "</div>"
-    echo "<div class='news'>". $news->getContent() . "</div>";
+    echo "<div class=\"news\">". $news->getContent() . "</div>";
     echo "</div>"
 }
 ?>
