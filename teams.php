e0b8e0de59077b32db119d17e772388d4ac92a3b
Matthew Pavia <matt.pavia@gmail.com>
2013-08-07 20:14:04 -0400

    Formatting

--- teams.php
+++ teams.php
@@ -31,34 +31,34 @@ if (isset($team)) {
     $teams = Team::getTeams();
 ?>
 <div class="teampage_content">
-<table class="teams_table">
-<tr class="teams_tr">
-<th style="width:30%"> Name </th>
-<th> Rating </th>
-<th> Leader </th>
-<th> Members </th>
-<th> Matches </th>
-<th> Activity </th>
-</tr>
-<?php
-    foreach ($teams as $key => $id) {
-        $team = new Team($id);
-	echo "<tr class=\"teams_tr\">\n";
-        echo "<td><a href='" . $team->getURL() . "'>" . $team->getName() . "</td>\n";
-        echo "<td>" . $team->getElo() . "</td>\n";
-        $leader = $team->getLeader();
-        echo "<td>" . $leader['username'] . "</td>\n";
-        echo "<td> " . $team->getNumMembers() . "</td>\n";
-        echo "<td><a href='" . $team->getMatchesURL() . "'>" . $team->getNumTotalMatches() . "</a></td>\n";
-        echo "<td>" . $team->getActivity() . "</td>\n";
-    	echo "</tr>\n";
+    <table class="teams_table">
+    <tr class="teams_tr">
+        <th style="width:30%"> Name </th>
+        <th> Rating </th>
+        <th> Leader </th>
+        <th> Members </th>
+        <th> Matches </th>
+        <th> Activity </th>
+    </tr>
+    <?php
+        foreach ($teams as $key => $id) {
+            $team = new Team($id);
+            echo "<tr class=\"teams_tr\">\n";
+            echo "<td><a href='" . $team->getURL() . "'>" . $team->getName() . "</td>\n";
+            echo "<td>" . $team->getElo() . "</td>\n";
+            $leader = $team->getLeader();
+            echo "<td>" . $leader['username'] . "</td>\n";
+            echo "<td> " . $team->getNumMembers() . "</td>\n";
+            echo "<td><a href='" . $team->getMatchesURL() . "'>" . $team->getNumTotalMatches() . "</a></td>\n";
+            echo "<td>" . $team->getActivity() . "</td>\n";
+        	echo "</tr>\n";
+        }
+
     }
+    ?>
 
-}
-?>
-
-</table>
-</div>
+    </table> <!-- end .teams_table -->
+</div> <!-- end .teampage_content -->
 <?php
 $footer = new Footer();
 $footer->draw();
