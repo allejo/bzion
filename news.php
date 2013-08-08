5fbf446b3168eddd35ab461c27589df0c725013f
Ashvala Vinay <mail@ashvala.net>
2013-08-07 22:13:45 -0400

    another ode to Ashvala's impeccable stupidity...

--- news.php
+++ news.php
@@ -12,14 +12,14 @@ $newsArticles = News::getNews();
 <?php
 foreach ($newsArticles as $key => $id) {
     $news = new News($id);
-    echo "<div class=\"news_box\">\n"
-    echo "<div class=\"title_box\">\n"
+    echo "<div class=\"news_box\">\n";
+    echo "<div class=\"title_box\">\n";
     echo "<div id=\"news_title\">" . $news->getSubject() . "</div>\n";
     $author = new Player($news->getAuthor());
     echo "<div id=\"author\">By " . $author->getUsername() . " at " . $news->getUpdated() . "</div>\n";
-    echo "</div>\n"
+    echo "</div>\n";
     echo "<div class=\"news\">". $news->getContent() . "</div>\n";
-    echo "</div>\n"
+    echo "</div>\n";
 }
 ?>
 </div>
