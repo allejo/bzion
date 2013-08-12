9dfdcaf0b2049c4b969062d3eb73b6c046389e04
Matthew Pavia <matt.pavia@gmail.com>
2013-08-12 16:02:29 -0400

    Add style to bans page

--- bans.php
+++ bans.php
@@ -8,15 +8,30 @@ $header->draw();
 
 $banList = Ban::getBans();
 
+?>
+
+<div class="bans"> 
+
+<?php
+
 foreach ($banList as $key => $id) {
     $ban = new Ban($id);
     $bannedPlayer = new Player($ban->getPlayer());
-    echo "<h4>" . $bannedPlayer->getUsername() . "</h4>";
+    echo "<div class=\"ban_box\">\n";
+    echo "<div class=\"ban_title_box\">\n";
+    echo "<div class=\"ban_title\">" . $bannedPlayer->getUsername() . "</div>\n";
     $author = new Player($ban->getAuthor());
-    echo "<small>By " . $author->getUsername() . " at " . $ban->getUpdated() . "</small><br />";
-    echo $ban->getReason() . "<br />";
-    echo "<br />";
+    echo "</div>\n";
+    echo "<div class=\"ban_content\">". $ban->getReason() . "\n";
+    echo "<div class=\"ban_author\">By <a href=\"" . $author->getURL() . "\">" . $author->getUsername() . "</a> at " . $ban->getUpdated() . "</div></div>\n";
+    echo "</div>\n";
 }
+
+?>
+
+</div> <!-- end .bans -->
+
+<?php
 
 $footer = new Footer();
 $footer->draw();
