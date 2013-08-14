e3701e2b42f4de4d8acb23076cc1352a4c05c243
Matthew Pavia <matt.pavia@gmail.com>
2013-08-13 21:57:54 -0400

    Add content to messages.php to display groups and messages

--- messages.php
+++ messages.php
@@ -10,11 +10,64 @@ if (!isset($_SESSION['username'])) {
 
 $header->draw("Messages");
 
-$messages = Message::getMessages($_SESSION['bzid']);
+$groups = Group::getGroups($_SESSION['bzid']);
 
-echo "<pre>";
-print_r($messages);
-echo "</pre>";
+?>
+
+<div class="groups">
+
+<div class="groups_toolbar">
+    <div class="groups_toolbar_option"><a href="#">Active</a></div>
+    <div class="groups_toolbar_option"><a href="#">Inactive</a></div>
+    <div class="groups_toolbar_option"><a href="#">Other</a></div>
+</div>
+
+<table class="group_list">
+
+<?php
+
+foreach ($groups as $key => $id) {
+    $group = new Group($id);
+
+    date_default_timezone_set('America/New_York');
+    echo "<tr><td><a href='" . $group->getURL() . "'>";
+    echo "<div class='group_subject'>" . $group->getSubject() . "</div>";
+    echo "<div class='group_last_activity'>" . $group->getLastActivity() . "</div>";
+    echo "<div style='clear:both'>";
+    echo "<div class='content_one'>Some Content</div>";
+    echo "<div class='content_two'>Some More Content</div>";
+    echo "</a></td></tr>";
+}
+
+?>
+
+</table> <!-- end .group_list -->
+
+</div> <!-- end .groups -->
+
+<?php
+
+if (isset($_GET['id'])) {
+    $messages = Message::getMessages($_GET['id']);
+    ?>
+    <div class="group_message">
+        <div class="group_message_toolbar">
+            <div class="group_message_option"><a href="#">Compose</a></div>
+            <div class="group_message_option"><a href="#">Delete</a></div>
+            <div class="group_message_option_disabled">Respond</div>
+            <div class="group_message_option_disabled">Forward</div>
+        </div>
+        <div class="group_message_content">
+            <?php
+            echo "<pre>";
+            print_r($messages);
+            echo "</pre>";
+            ?>
+        </div>
+    </div> <!-- end .group_message -->
+
+<?php
+}
 
 $footer = new Footer();
 $footer->draw();
