2536364cd69f85e69c3c6c0213005bfa3b28d336
kongr45gpen <kongr45gpen@helit.org>
2013-08-16 18:55:08 +0300

    ajax/sendMessage.php: Support the creation of new groups

--- ajax/sendMessage.php
+++ ajax/sendMessage.php
@@ -12,24 +12,38 @@ try {
     if (!isset($_SESSION['username'])) {
         throw new Exception("You need to be logged in to do this.");
     }
-
-    if (!isset($_POST['to']) || !isset($_POST['content'])) {
-        throw new Exception("Bad request");
-    }
-
-    $group_to = new Group($_POST['to']);
-    $content  = $_POST['content'];
     $bzid     = $_SESSION['bzid'];
 
-    if (!$group_to->isValid()) {
-        throw new Exception("The group you specified does not exist.");
+    // Two different POST variable layouts are acceptable:
+    //
+    // 1. ?content=foo&group_to=123 (To send a response to an already existing group)
+    // 2. ?content=foo&to=123,456,789&subject=bar (To create a new message group)
+    if (!isset($_POST['content'])) {
+        throw new Exception("Bad request");
     }
+    $content = $_POST['content'];
 
-    if (!$group_to->isMember($bzid)) {
-        throw new Exception("You aren't a member of that group.");
+    if (isset($_POST['group_to'])) {
+        // Send a message to a group
+        $group_to = new Group($_POST['group_to']);
+
+        if (!$group_to->isValid()) {
+            throw new Exception("The group you specified does not exist.");
+        }
+
+        if (!$group_to->isMember($bzid)) {
+            throw new Exception("You aren't a member of that group.");
+        }
+
+        Message::sendMessage($group_to->getId(), $_SESSION['bzid'], $content);
+    } elseif (!isset($_POST['to']) || !isset($_POST['subject'])) {
+        throw new Exception("Bad request");
+    } else {
+        // Create a group and send a message to it
+        $group_to = Group::createGroup(htmlspecialchars($_POST['subject']), explode(",", $_POST['to']));
+
+        Message::sendMessage($group_to->getId(), $_SESSION['bzid'], $content);
     }
-
-    Message::sendMessage($group_to->getId(), $_SESSION['bzid'], $content);
 
 } catch (Exception $e) {
     $error = true;
