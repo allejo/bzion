8221fc648b6523a8f1b924836259e55fac097bb5
kongr45gpen <kongr45gpen@helit.org>
2013-08-16 18:16:03 +0300

    ajax/sendMessage.php: Use exceptions to handle errors

--- ajax/sendMessage.php
+++ ajax/sendMessage.php
@@ -2,27 +2,38 @@
 
 include("../bzion-load.php");
 
-$header = new Header();
+$error = false;
+$message = "Your message was sent successfully";
 
-if (!isset($_SESSION['username'])) {
-    die("You need to be logged in to do this.");
+try {
+
+    $header = new Header();
+
+    if (!isset($_SESSION['username'])) {
+        throw new Exception("You need to be logged in to do this.");
+    }
+
+    if (!isset($_GET['to']) || !isset($_GET['content'])) {
+        throw new Exception("Bad request");
+    }
+
+    $group_to = new Group($_GET['to']);
+    $content  = $_GET['content'];
+    $bzid     = $_SESSION['bzid'];
+
+    if (!$group_to->isValid()) {
+        throw new Exception("The group you specified does not exist.");
+    }
+
+    if (!$group_to->isMember($bzid)) {
+        throw new Exception("You aren't a member of that group.");
+    }
+
+    Message::sendMessage($group_to->getId(), $_SESSION['bzid'], $content);
+
+} catch (Exception $e) {
+    $error = true;
+    $message = $e->getMessage();
 }
 
-if (!isset($_GET['to']) || !isset($_GET['content'])) {
-    die("Bad request");
-}
-
-$group_to = new Group($_GET['to']);
-$content  = $_GET['content'];
-$bzid     = $_SESSION['bzid'];
-
-if (!$group_to->isValid()) {
-    die("The group you specified does not exist");
-}
-
-if (!$group_to->isMember($bzid)) {
-    die("You aren't a member of that group");
-}
-
-// TODO: Check if the user belongs in that group
-Message::sendMessage($group_to->getId(), $_SESSION['bzid'], $content);
+echo $message;
