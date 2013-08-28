84316ee9d1360f9911728a409567847402abfae6
kongr45gpen <electrovesta@gmail.com>
2013-08-28 12:41:17 +0300

    Show link to the team on the profile page of each player

--- players.php
+++ players.php
@@ -15,8 +15,15 @@ if (isset($player)) {
     if ($player->isValid()) {
         $header->draw("Players :: " . $player->getUsername());
 
+        $playerTeam = $player->getTeam();
+        $teamlink = $playerTeam->getName();
+
+        if ($playerTeam->isValid()) {
+            $teamlink = '<a href="' . $playerTeam->getURL() . '">' . $teamlink . '</a>';
+        }
+
         echo "<b>" . $player->getUsername() . "</b><br />";
-        echo "Team: " . $player->getTeam()->getName() . "<br />";
+        echo "Team: $teamlink<br />";
         echo "Joined: " . $player->getJoinedDate() . "<br />";
     } else {
         $header->draw("Players");
