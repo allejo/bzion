e35467438f70c473855025b65480f2de976c45ea
kongr45gpen <kongr45gpen@helit.org>
2013-09-02 15:46:41 +0300

    Add a testFile.sh script which tests PHP files for errors

---
+++ tests/testFile.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+ERRORLOG="error.log"
+
+GREY="\033[0;37m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+CYAN="\033[0;36m"
+
+ERROR=0
+
+NO_COLOR="\033[0;39m"
+
+ERROR_LOG_CONTENTS=""
+
+for file
+do
+    [ -f $ERRORLOG ] && rm $ERRORLOG
+
+    php $file > /dev/null 2> $ERRORLOG
+
+    [ -f $ERRORLOG ] || continue
+
+    notices=`grep -i notice $ERRORLOG | wc -l`
+    warnings=`grep -i warning $ERRORLOG | wc -l`
+    fatal=`grep -i fatal $ERRORLOG | wc -l`
+    parse=`grep -i parse $ERRORLOG | wc -l`
+    strict=`grep -i 'strict\|deprecated' $ERRORLOG | wc -l`
+
+    message=()
+    color=$GREEN
+
+    if [[ $notices -ne 0 ]]; then
+      message+=("$notices notices")
+    fi
+    if [[ $warnings -ne 0 ]]; then
+      message+=("$warnings warnings")
+    fi
+    if [[ $strict -ne 0 ]]; then
+      message+=("$strict strict warnings")
+      color=$YELLOW
+      ERROR=1
+    fi
+    if [[ $fatal -ne 0 ]]; then
+      message+=("$fatal fatal errors")
+      color=$RED
+      ERROR=2
+    fi
+    if [[ $parse -ne 0 ]]; then
+      message+=("$parse parse errors")
+      color=$RED
+      ERROR=3
+    fi
+
+
+    ERROR_LOG_CONTENTS+="\n$CYAN""Error log for file $file:$NO_COLOR\n"
+    ERROR_LOG_CONTENTS+=`cat $ERRORLOG`
+
+    result=$(printf ", %s" "${message[@]}")
+    result=${result:2}
+
+
+
+    if [ -z "$result" ]; then
+        result="No errors found"
+    fi
+
+    echo -e "$color$file:\t$result$NO_COLOR"
+done
+
+echo -e "$ERROR_LOG_CONTENTS"
+
+exit $ERROR
