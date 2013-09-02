7a3986a9d1240490347facad5ff237ff7d8e8727
kongr45gpen <kongr45gpen@helit.org>
2013-09-02 15:58:57 +0300

    Use a runTests.sh file

---
+++ tests/runTests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+phpunit --coverage-text --bootstrap bzion-load.php --colors tests
+PHPUNIT=$?
+
+FILES="`find . -maxdepth 1 -iname '*.php' -and ! -iname 'bzion*' | sort`"
+
+tests/testFile.sh $FILES
+FILETEST=$?
+
+
+if [[ $PHPUNIT -ne 0 ]]; then
+    exit 1
+fi
+
+if [[ $FILETEST -ne 0 ]]; then
+    exit 2
+fi
