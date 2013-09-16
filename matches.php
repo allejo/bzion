e91ec372db8bdda595b29d9c525baab0d098b7d3
Matthew Pavia <matt.pavia@gmail.com>
2013-09-16 16:36:54 -0400

    Add some links to the matches page

--- matches.php
+++ matches.php
@@ -52,11 +52,11 @@ if (isset($team)) {
         $team_b = $match->getTeamB();
         echo "<tr>";
         echo "<td>" . $match->getTimestamp() . "</td>";
-        echo "<td>" . $team_a->getName() . " vs " . $team_b->getName() . "</td>";
+        echo "<td><a href='" . $team_a->getURL() . "'>" . $team_a->getName() . "</a> vs <a href='" . $team_b->getURL() . "'>" . $team_b->getName() . "</a></td>";
         echo "<td>" . $match->getTeamAPoints() . " - " . $match->getTeamBPoints() . "</td>";
         echo "<td>" . $match->getDuration() . " min</td>";
         $reporter = new Player($match->getEnteredBy());
-        echo "<td>" . $reporter->getUsername() . "</td>";
+        echo "<td><a href='" . $reporter->getURL() . "'>" . $reporter->getUsername() . "</a></td>";
         echo "</tr>";
     }
     ?>
