345ea624464b5b85a40a12c5e31844004e243183
Matthew Pavia <matt.pavia@gmail.com>
2013-09-18 02:57:45 -0400

    Add profile page

---
+++ profile.php
@@ -0,0 +1,28 @@
+<?php
+
+include("bzion-load.php");
+
+$header = new Header();
+$header->draw("Profile");
+
+$me = new Player($_SESSION['bzid']);
+
+$playerTeam = $me->getTeam();
+$teamlink = $playerTeam->getName();
+
+if ($playerTeam->isValid()) {
+    $teamlink = '<a href="' . $playerTeam->getURL() . '">' . $teamlink . '</a>';
+}
+
+echo "<h2>" . $me->getUsername() . "</h2><br />";
+echo "Team: $teamlink<br />";
+echo "Joined: " . $me->getJoinedDate() . "<br />";
+
+echo "<br />More content coming soon...<br />";
+
+echo "<br /><a href='#'>Edit your profile...</a>";
+
+$footer = new Footer();
+$footer->draw();
+
+?>
