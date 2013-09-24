653f8ee755e5756dd96dc1e84c44354f1490872b
kongr45gpen <electrovesta@gmail.com>
2013-09-24 17:40:16 +0300

    Override Carbon's diffForHumans() method so that the word "now" is output for small durations

--- classes/TimeDate.php
+++ classes/TimeDate.php
@@ -1,5 +1,9 @@
 <?php
 
 class TimeDate extends Carbon\Carbon {
-
+    public function diffForHumans(Carbon\Carbon $other = null) {
+        if (self::diffInSeconds($other, true) < 4)
+                return "now";
+        return parent::diffForHumans($other);
+    }
 }
