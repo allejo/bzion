247208af6284abf1cd000ceabac97641a0f38fd2
kongr45gpen <kongr45gpen@helit.org>
2013-09-29 15:12:35 +0300

    Do not show a list of previous messages when writing a response, and make sure that the "Enter" key can be used as well to submit the form

--- messages.php
+++ messages.php
@@ -171,9 +171,9 @@ if (!$messages) {
 
     </div> <!-- end .group_message_scroll -->
 
-    <form class="alt_compose_form">
+    <form class="alt_compose_form" autocomplete="off">
         <input type="text" id="composeArea" class="input_compose_area" placeholder="Enter your message here..." />
-        <button id="composeButton" onclick="sendResponse()" type="button" class="ladda-button" data-style="zoom-out" data-size="xs">
+        <button id="composeButton" onclick="sendResponse()" type="submit" class="ladda-button" data-style="zoom-out" data-size="xs">
             <span class="ladda-label">Submit</span>
         </button>
     </form> <!-- end .alt_compose_form -->
