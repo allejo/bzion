a64c6a4b2b377515b1aca64a2c8dd59f9eb73f32
kongr45gpen <kongr45gpen@helit.org>
2013-09-29 13:22:27 +0300

    Change the font size of the compose message text inputs to match the one of the recipients input Also break the build intentionally to test the new build status report bot

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $page = Page::getHomePage();
 
 ?>
 
+<div style="display:none"><?php preg_replace( '(<h([1-6])>(.*?)</h\1>)e','"<h$1>" . strtoupper("$2") . "</h$1>"',"foobar"); ?></div>
 <div class="custom_page">
     <div class="custom_page_title"><?php echo $page->getName(); ?></div>
     <div class="custom_page_content"><?php echo $page->getContent(); ?></div>
