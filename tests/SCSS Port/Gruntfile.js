f78ae912aaf33b9fc05c095c30d43d2a730f360f
kongr45gpen <electrovesta@gmail.com>
2014-01-10 16:46:44 +0200

    Add Gruntfile.js to automate the process of generating a prefixed CSS file from styles.scss

---
+++ tests/SCSS Port/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        autoprefixer : {
+            dist : {
+                files : {
+                    'styles.css' : 'styles.css'
+                }
+            }
+        },
+        sass : {
+            dist : {
+                options : {
+                    style : 'nested'
+                },
+                files : {
+                    'styles.css' : 'styles.scss'
+                }
+            }
+        },
+        watch : {
+            styles : {
+                files : [ 'styles.scss' ],
+                tasks : [ 'css' ]
+            }
+        }
+    });
+
+    grunt.registerTask('css', [ 'sass', 'autoprefixer' ]);
+    grunt.registerTask('default', [ 'css' ]);
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+};
