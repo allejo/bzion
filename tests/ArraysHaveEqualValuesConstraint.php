8bf788f36ec1632e75587f55fced16567138451b
kongr45gpen <electrovesta@gmail.com>
2014-01-30 22:15:32 +0200

    Add more tests for the Team class and perform fixes where necessary

---
+++ tests/ArraysHaveEqualValuesConstraint.php
@@ -0,0 +1,48 @@
+<?php
+
+class ArraysHaveEqualValuesConstraint extends PHPUnit_Framework_Constraint {
+
+    /**
+     *
+     * @var array|ArrayAccess
+     */
+    protected $value;
+
+    /**
+     *
+     * @param array|ArrayAccess $value
+     * @throws PHPUnit_Framework_Exception
+     */
+    public function __construct($value) {
+        if (!(is_array($value) || $value instanceof ArrayAccess)) {
+            throw PHPUnit_Util_InvalidArgumentHelper::factory(1, 'array or ArrayAccess');
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * Evaluates the constraint for parameter $other.
+     * Returns TRUE if the constraint is met, FALSE otherwise.
+     *
+     * @param mixed $other
+     *            Value or object to evaluate.
+     * @return bool
+     */
+    public function matches($other) {
+        if (count($this->value) != count($other))
+            return false;
+
+        return empty(array_diff($this->value, $other));
+    }
+
+    /**
+     * Returns a string representation of the constraint.
+     *
+     * @return string
+     */
+    public function toString() {
+        return 'has the same values with ' .
+                   PHPUnit_Util_Type::export($this->value);
+    }
+}
