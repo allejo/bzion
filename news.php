d672be6e4bcfca6551cd5671f0e7d5f57af3ad2a
Vladimir Jimenez <allejo@users.noreply.github.com>
2014-03-11 13:24:08 -0700

    removed white space

--- news.php
+++ news.php
@@ -7,19 +7,18 @@ $header->draw();
 
 $newsArticles = News::getNews();
 
-
 foreach ($newsArticles as $key => $id)
 {
     $news = new News($id);
     $author = $news->getAuthor();
 
     echo '<article>';
-    echo '       <h1>' . $news->getSubject() . '</h1>';
-    echo '       <p>' . $news->getContent() . '</p>';
-    echo '       <footer>';
-    echo '           Posted by <a href="' . $author->getURL() . '">' . $author->getUsername() . '</a> ' . $news->getUpdated();
-    echo '       </footer>';
-    echo '   </article>';
+    echo '    <h1>' . $news->getSubject() . '</h1>';
+    echo '    <p>' . $news->getContent() . '</p>';
+    echo '    <footer>';
+    echo '        Posted by <a href="' . $author->getURL() . '">' . $author->getUsername() . '</a> ' . $news->getUpdated();
+    echo '    </footer>';
+    echo '</article>';
 }
 
 $footer = new Footer();
