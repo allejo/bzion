9ded6ae8552622fd821ea2c8aacee97f2f216870
kongr45gpen <electrovesta@gmail.com>
2014-03-14 00:08:18 +0200

    Fix Routes

--- index.php
+++ index.php
@@ -14,12 +14,22 @@ $routes->add('bans', new Route('/bans', array('controller' => 'bans')));
 $routes->add('login', new Route('/login', array('controller' => 'login')));
 $routes->add('logout', new Route('/logout', array('controller' => 'logout')));
 $routes->add('matches', new Route('/matches', array('controller' => 'matches')));
+
 $routes->add('messages', new Route('/messages', array('controller' => 'messages')));
+$routes->add('view_discussion', new Route('/messages/{id}', array('controller' => 'messages')));
+
 $routes->add('news', new Route('/news', array('controller' => 'news')));
+
 $routes->add('players', new Route('/players', array('controller' => 'players')));
+$routes->add('view_player', new Route('/players/{slug}', array('controller' => 'players')));
+
 $routes->add('profile', new Route('/profile', array('controller' => 'profile')));
 $routes->add('servers', new Route('/servers', array('controller' => 'servers')));
+
 $routes->add('teams', new Route('/teams', array('controller' => 'teams')));
+$routes->add('view_team', new Route('/teams/{slug}', array('controller' => 'teams')));
+
+$routes->add('custom_page', new Route('/{slug}', array('controller' => 'pages')));
 
 
 $context = new RequestContext();
