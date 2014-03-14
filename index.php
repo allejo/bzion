2c3b801a26ae5f52e770d97e26ff92fd9d24d06a
kongr45gpen <electrovesta@gmail.com>
2014-03-14 16:41:46 +0200

    Move routes into a yml file

--- index.php
+++ index.php
@@ -2,42 +2,28 @@
 
 require_once("bzion-load.php");
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Router;
 
-$routes = new RouteCollection();
-$routes->add('index', new Route('/', array('controller' => 'index')));
-$routes->add('bans', new Route('/bans', array('controller' => 'bans')));
-$routes->add('login', new Route('/login', array('controller' => 'login')));
-$routes->add('logout', new Route('/logout', array('controller' => 'logout')));
-$routes->add('matches', new Route('/matches', array('controller' => 'matches')));
+$locator = new FileLocator(array(__DIR__));
 
-$routes->add('messages', new Route('/messages', array('controller' => 'messages')));
-$routes->add('view_discussion', new Route('/messages/{id}', array('controller' => 'messages')));
+$request = Request::createFromGlobals();
+$requestContext = new RequestContext();
+$requestContext->fromRequest($request);
 
-$routes->add('news', new Route('/news', array('controller' => 'news')));
+$router = new Router(
+    new YamlFileLoader($locator),
+    'routes.yml',
+    array('cache_dir' => __DIR__.'/cache'),
+    $requestContext
+);
 
-$routes->add('players', new Route('/players', array('controller' => 'players')));
-$routes->add('view_player', new Route('/players/{slug}', array('controller' => 'players')));
+$parameters = $router->matchRequest($request);
 
-$routes->add('profile', new Route('/profile', array('controller' => 'profile')));
-$routes->add('servers', new Route('/servers', array('controller' => 'servers')));
-
-$routes->add('teams', new Route('/teams', array('controller' => 'teams')));
-$routes->add('view_team', new Route('/teams/{slug}', array('controller' => 'teams')));
-
-$routes->add('custom_page', new Route('/{slug}', array('controller' => 'pages')));
-
-
-$context = new RequestContext();
-$context->fromRequest(Request::createFromGlobals());
-
-$matcher = new UrlMatcher($routes, $context);
-
-$parameters = $matcher->matchRequest(Request::createFromGlobals());
-
-
-require ("controllers/" . $parameters['controller'] . ".php");
+require ("controllers/" . $parameters['_controller'] . ".php");
