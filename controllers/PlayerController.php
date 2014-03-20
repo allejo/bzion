db7595e8717607f7b1cf81295b69323176a75cb8
kongr45gpen <electrovesta@gmail.com>
2014-03-20 16:50:28 +0200

    Add Player view

--- controllers/PlayerController.php
+++ controllers/PlayerController.php
@@ -3,57 +3,12 @@
 class PlayerController extends HTMLController {
 
     public function showAction(Player $player) {
-        if ($player->isValid()) {
-            $this->drawHeader("Players :: " . $player->getUsername());
-
-            $playerTeam = $player->getTeam();
-            $teamlink = $playerTeam->getName();
-
-            if ($playerTeam->isValid()) {
-                $teamlink = '<a href="' . $playerTeam->getURL() . '">' . $teamlink . '</a>';
-            }
-            echo "<div class='profile_page'>";
-            echo "<img class='avatar_player' src='".$player->getAvatar() . "'</div>";
-            echo "<div class='label player_name'>" . $player->getUsername() . "</div>";
-            echo "<div class='label label_team'> Team:" .  $teamlink . "</div>";
-            echo "<div class='label'> Joined: " . $player->getJoinedDate() . "</div>";
-            echo "<div class='descript_player'>", $player->getDescription() ,"</div>";
-            echo "</div>";
-        } else {
-            $this->drawHeader("Players");
-
-            echo "The specified player could not be found. <br />";
+        if ($this->validate($player)) {
+            return array("player" => $player);
         }
     }
 
     public function listAction() {
-        $this->drawHeader("Players");
-
-        $players = Player::getPlayers();
-        ?>
-
-        <div class="table players">
-            <ul>
-                <li>Name</li>
-                <li>Team</li>
-                <li>Joined</li>
-            </ul>
-
-        <?php
-            foreach ($players as $player)
-            {
-                echo '<ul>';
-                echo '    <li><a href="' . $player->getURL() . '">' . $player->getUsername() . '</a></li>';
-                $teamlink = $player->getTeam()->getName();
-                if ($player->getTeam()->isValid()) {
-                    $teamlink = '<a href="' . $player->getTeam()->getURL() . '">' . $teamlink . '</a>';
-                }
-                echo '    <li>' . $teamlink . '</li>';
-                echo '    <li>' . $player->getJoinedDate() . '</li>';
-                echo '</ul>';
-            }
-            echo '</div>';
-
-
+        return array("players" => Player::getPlayers());
     }
 }
