7bae5245786a742d04593659ddb2a7c1c8e2c6e7
kongr45gpen <electrovesta@gmail.com>
2014-03-20 21:19:36 +0200

    Fix javascript handling for templates

--- controllers/MatchController.php
+++ controllers/MatchController.php
@@ -2,14 +2,6 @@
 
 class MatchController extends HTMLController {
 
-    public function cleanup() {
-        $footer = new Footer();
-
-        $footer->addScript("assets/js/matches.js");
-
-        $footer->draw();
-    }
-
     public function listByTeamAction(Team $team) {
         return $this->render("Match/list.html.twig",
                array ("matches" => $team->getMatches(), "team" => $team));
