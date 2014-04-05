8d694ceb445ed6d8162c1245f515835cae16a659
allejo <allejo@me.com>
2014-04-05 00:47:04 -0700

    Added phpDoc to Permission model

--- models/Permission.php
+++ models/Permission.php
@@ -11,7 +11,16 @@
  */
 class Permission extends Model
 {
+    /**
+     * The name of the permission
+     * @var string
+     */
     private $name;
+
+    /**
+     * The description of the permission
+     * @var string
+     */
     private $description;
 
     /**
@@ -19,6 +28,11 @@ class Permission extends Model
      */
     const TABLE = "permissions";
 
+    /**
+     * Construct a new Permission
+     *
+     * @param int $perm_name The name of the permission
+     */
     public function __construct($perm_name)
     {
         parent::__construct($perm_name, "name");
@@ -30,11 +44,19 @@ class Permission extends Model
         $this->description = $permission['description'];
     }
 
+    /**
+     * Get the description of the permission
+     * @return string The description of the permission
+     */
     public function getDescription()
     {
         return $this->description;
     }
 
+    /**
+     * Get the name of the permission
+     * @return string The name of the permission
+     */
     public function getName()
     {
         return $this->name;
