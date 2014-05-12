94c05a15cd87dcafc041debac65a3731923a47e4
allejo <allejo@me.com>
2014-05-12 03:40:36 -0700

    Added view for single news item

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,6 +1,10 @@
 <?php
 
-class NewsController extends HTMLController {
+class NewsController extends HTMLController
+{
+    public function showAction($id) {
+        return array("article" => new News($id));
+    }
 
     public function listAction() {
         return array("news" => News::getNews());
