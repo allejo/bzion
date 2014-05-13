a1b3d91180a53fc4b43e284918afeb9a85af5366
kongr45gpen <electrovesta@gmail.com>
2014-05-13 15:48:32 +0300

    De-break permissions and roles

--- DATABASE.sql
+++ DATABASE.sql
@@ -193,7 +193,45 @@ CREATE TABLE `permissions` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+INSERT INTO `permissions` (`id`, `name`, `description`)
+VALUES
+       (1,'add_user','The ability to create users'),
+       (2,'edit_user','The ability to edit user profile settings'),
+       (3,'del_user','The ability to delete a user from the website (deactivate a user)'),
+       (4,'wipe_user','The ability to entirely wipe a user from the database'),
+       (5,'add_team','The ability to create a new team'),
+       (6,'edit_team','The ability to edit any team without being the leader of it'),
+       (7,'del_team','The ability to mark a team as deleted without being the leader of it'),
+       (8,'wipe_team','The ability to entirely wipe a team from the database'),
+       (9,'add_match','The ability to enter a match'),
+       (10,'edit_match','The ability to edit an exisitng match'),
+       (11,'del_match','The ability to mark a match as deleted'),
+       (12,'wipe_match','The ability to wipe a match from the database'),
+       (13,'add_page','The ability to create new pages for the website as drafts'),
+       (14,'edit_page','The ability to edit existing pages on the website'),
+       (15,'del_page','The ability to mark a page as deleted on the website'),
+       (16,'wipe_page','The ability to wipe a page from the database'),
+       (17,'publish_page','The ability to publish a page marked as a draft'),
+       (18,'add_news','The ability to create news content as drafts'),
+       (19,'publish_news','The ability to publish news articles'),
+       (20,'edit_news','The abliity to edit news articles written by other people'),
+       (21,'del_news','The abliity to delete news articles written by the author or other users'),
+       (22,'wipe_news','The ability to wipe a news article from the database'),
+       (23,'add_ban','The ability to create a new ban'),
+       (24,'edit_ban','The ability to edit an existing ban'),
+       (25,'del_ban','The ability to mark a ban as deleted'),
+       (26,'wipe_ban','The ability to wipe a ban from the database'),
+       (27,'add_server','The ability to add a new server to the list of official league servers'),
+       (28,'edit_server','The ability to edit an existing server'),
+       (29,'del_server','The ability to mark a server as deleted'),
+       (30,'wipe_server','The ability to wipe a server from the database'),
+       (31,'add_role','The ability to create new roles for users'),
+       (32,'edit_role','The ability to edit existing roles for users'),
+       (33,'del_role','The ability to mark a role as deleted'),
+       (34,'wipe_role','The ability to wipe a role from the database'),
+       (35,'send_pm','The ability to send private messages'),
+       (36,'view_visitor_log','The ability to see a player\'s visits and their IP addresses'),
+       (37,'view_server_list','The ability to view the available official league servers');
 
 # Dump of table player_groups
 # ------------------------------------------------------------
@@ -215,8 +253,8 @@ CREATE TABLE `player_roles` (
   `role_id` int(10) unsigned NOT NULL,
   KEY `user_id` (`user_id`),
   KEY `role_id` (`role_id`),
-  CONSTRAINT `player_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `players` (`id`),
-  CONSTRAINT `player_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+  CONSTRAINT `player_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `players` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `player_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -257,6 +295,118 @@ CREATE TABLE `role_permission` (
   CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`perm_id`) REFERENCES `permissions` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `role_permission` (`role_id`, `perm_id`)
+VALUES
+       (1,1),
+       (1,2),
+       (1,3),
+       (1,4),
+       (1,5),
+       (1,6),
+       (1,7),
+       (1,8),
+       (1,9),
+       (1,10),
+       (1,11),
+       (1,12),
+       (1,13),
+       (1,14),
+       (1,15),
+       (1,16),
+       (1,17),
+       (1,18),
+       (1,19),
+       (1,20),
+       (1,21),
+       (1,22),
+       (1,23),
+       (1,24),
+       (1,25),
+       (1,26),
+       (1,27),
+       (1,28),
+       (1,29),
+       (1,30),
+       (1,31),
+       (1,32),
+       (1,33),
+       (1,34),
+       (1,35),
+       (1,36),
+       (1,37),
+       (5,1),
+       (5,2),
+       (5,3),
+       (5,5),
+       (5,6),
+       (5,7),
+       (5,9),
+       (5,10),
+       (5,11),
+       (5,13),
+       (5,14),
+       (5,15),
+       (5,17),
+       (5,18),
+       (5,19),
+       (5,20),
+       (5,21),
+       (5,23),
+       (5,24),
+       (5,25),
+       (5,27),
+       (5,28),
+       (5,29),
+       (5,31),
+       (5,32),
+       (5,33),
+       (5,35),
+       (5,36),
+       (5,37),
+       (2,1),
+       (2,2),
+       (2,3),
+       (2,5),
+       (2,6),
+       (2,7),
+       (2,9),
+       (2,10),
+       (2,11),
+       (2,13),
+       (2,14),
+       (2,15),
+       (2,17),
+       (2,18),
+       (2,19),
+       (2,20),
+       (2,21),
+       (2,23),
+       (2,24),
+       (2,25),
+       (2,35),
+       (2,36),
+       (2,37),
+       (3,5),
+       (3,6),
+       (3,9),
+       (3,10),
+       (3,13),
+       (3,14),
+       (3,18),
+       (3,20),
+       (3,35),
+       (3,36),
+       (3,37),
+       (4,5),
+       (4,9),
+       (4,10),
+       (4,35),
+       (4,37),
+       (6,5),
+       (6,35),
+       (6,37),
+       (7,5),
+       (7,37);
 
 
 # Dump of table roles
@@ -270,7 +420,15 @@ CREATE TABLE `roles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+INSERT INTO `roles` (`id`, `name`, `reusable`, `protected`)
+VALUES
+    (1,'Developer',1,1),
+    (2,'Administrator',1,1),
+    (3,'Cop',1,1),
+    (4,'Referee',1,1),
+    (5,'System Administrator',1,1),
+    (6,'Player',1,1),
+    (7,'Player - No Private Messages',1,1);
 
 # Dump of table servers
 # ------------------------------------------------------------
