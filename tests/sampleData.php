d22bfcc665262fa9b9de102a401cc374d8951268
kongr45gpen <electrovesta@gmail.com>
2014-05-14 15:47:44 +0300

    Add dots to sampleData.php

--- tests/sampleData.php
+++ tests/sampleData.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require_once(__DIR__ . "/../bzion-load.php");
@@ -29,6 +30,7 @@ $kierra     = Player::newPlayer(2229, "kierra", 0, "active", Player::ADMIN);
 $mdskpr     = Player::newPlayer(8312, "mdskpr");
 $snake      = Player::newPlayer(54497, "Snake12534");
 $tw1sted    = Player::newPlayer(9736, "tw1sted", 0, "active", Player::DEVELOPER);
+dot();
 
 $olfm     = Team::createTeam("OpenLeague FM?", $kierra->getId(), "", "");
 $reptiles = Team::createTeam("Reptitles", $snake->getId(), "", "");
@@ -36,10 +38,12 @@ $fflood   = Team::createTeam("Formal Flood", $allejo->getId(), "", "");
 $lweak    = Team::createTeam("[LakeWeakness]", $mdskpr->getId(), "", "");
 $gsepar   = Team::createTeam("Good Separation", $tw1sted->getId(), "", "");
 $fradis   = Team::createTeam("Fractious disinclination", $ashvala->getId(), "", "");
+dot();
 
 $lweak->addMember($autoreport->getId());
 $fflood->addMember($blast->getId());
 $fradis->addMember($alezakos->getId());
+dot();
 
 Match::enterMatch($reptiles->getId(), $gsepar->getId(), 1, 9000, 17, $kierra->getId());
 Match::enterMatch($olfm->getId(), $lweak->getId(), 0, 0, 20, $blast->getId());
@@ -49,13 +53,16 @@ Match::enterMatch($olfm->getId(), $lweak->getId(), 5, 4, 20, $kierra->getId());
 Match::enterMatch($reptiles->getId(), $gsepar->getId(), 1, 1500, 20, $autoreport->getId());
 Match::enterMatch($olfm->getId(), $lweak->getId(), 1, 1, 30, $autoreport->getId());
 Match::enterMatch($fradis->getId(), $gsepar->getId(), 1, 2, 20, $kierra->getId());
+dot();
 
 $reptiles->update("activity", 9000, "i");
 $fflood->update("activity", -18, "i");
 $fradis->update("activity", 3.14159265358979323846, "d");
+dot();
 
 Server::addServer("BZPro Public HiX FFA", "bzpro.net:5154", $tw1sted->getId());
 Server::addServer("BZPro Public HiX Rabbit Chase", "bzpro.net:5155", $tw1sted->getId());
+dot();
 
 $group_to = Group::createGroup("New blog", array(
     $alezakos->getId(),
@@ -69,23 +76,30 @@ $group_to = Group::createGroup("New blog", array(
     $tw1sted->getId()
 ));
 Message::sendMessage($group_to->getId(), $snake->getId(), "Check out my new blog!");
+dot();
 
 Ban::addBan($snake->getId(), $alezakos->getId(), "2014-09-15", "Snarke 12534 has been barned again", "Cuz you're snake", "256.512.104.1");
 Ban::addBan($allejo->getId(), $tw1sted->getId(), "2014-05-17", "for using 'dope'", "dope", array("127.0.2.1", "128.0.3.2"));
 Ban::addBan($tw1sted->getId(), $alezakos->getId(), "2014-06-12", "tw1sted banned for being too awesome");
 Ban::addBan($alezakos->getId(), $tw1sted->getId(), "2014-11-01", "alezakos banned for breaking the build", "For breaking the build", array("256.512.124.1", "256.512.124.3"));
+dot();
 
 Page::addPage("Rules", "<p>This is a test page.</p>\n<p>Lets hope this works!</p>", $tw1sted->getId());
 Page::addPage("Contact", "<p>If you find anything wrong, please stop by irc.freenode.net channel #sujevo and let a developer know.<br /><br />Thanks", $tw1sted->getId());
+dot();
 
-News::addNews("Announcement", "Very important Announcement", $mdskpr->getId());
+$announcements = NewsCategory::addCategory("Announcements");
+$administration = NewsCategory::addCategory("Administration");
+$events = NewsCategory::addCategory("Events");
+$newFeatures = NewsCategory::addCategory("New Features");
+dot();
+
+News::addNews("Announcement", "Very important Announcement", $kierra->getId(), $newFeatures->getId());
 News::addNews("Cats think we are bigger cats", "In order for your indess recognizes where this whole mistake has come, and why one accuses the pleasure and praise the pain, and I will open to you all and set apart, what those founders of the truth and, as builders of the happy life himself has said about it. No one, he says, despise, or hate, or flee the desire as such, but because great pain to follow, if you do not pursue pleasure rationally. Similarly, the pain was loved as such by no one or pursues or desires, but because occasionally circumstances occur that one means of toil and pain can procure him some great pleasure to look verschaften be. To stay here are a trivial, so none of us would ever undertakes laborious physical exercise, except to obtain some advantage from it. But who is probably the blame, which requires an appetite, has no annoying consequences, or one who avoids a pain, which shows no desire? In contrast, blames and you hate with the law, which can soften and seduced by the allurements of present pleasure, without seeing in his blind desire which pain and inconvenience wait his reason. Same debt meet Those who from weakness, i.e to escape the work and the pain, neglect their duties. A person can easily and quickly make the real difference, to a quiet time where the choice of the decision is completely free and nothing prevents them from doing what we like best, you have to grasp every pleasure and every pain avoided, but to times it hits in succession of duties or guilty of factual necessity that you reject the desire and complaints must not reject. Why then the way will make a selection so that it Achieve a greater rejection by a desire for it or by taking over some pains to spare larger.", $alezakos->getId());
+dot();
 
-NewsCategory::addCategory("Announcements");
-NewsCategory::addCategory("Administration");
-NewsCategory::addCategory("Events");
-NewsCategory::addCategory("New Features");
+echo "\nThe database has been populated successfully.\n";
 
-echo "The database has been populated successfully.\n";
-
-?>
+function dot() {
+    echo ".";
+}
