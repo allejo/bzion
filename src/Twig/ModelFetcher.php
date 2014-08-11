7e4d5b9da3becb3aa322f2e2c4883c05ffc9b640
kongr45gpen <kongr45gpen@helit.org>
2014-08-11 20:28:56 +0300

    Add PHPDoc for the new ModelFetcher

--- src/Twig/ModelFetcher.php
+++ src/Twig/ModelFetcher.php
@@ -1,6 +1,11 @@
 <?php
 namespace BZIon\Twig;
 
+/**
+ * A twig global that enables the creation of Models from their IDs
+ *
+ * Example usage: `fetcher.newsCategory(15).id`
+ */
 class ModelFetcher
 {
     public function __call($type, $arguments)
