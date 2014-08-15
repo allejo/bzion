516bc1ce1f66464317515fcabbfebe97d6879367
kongr45gpen <kongr45gpen@helit.org>
2014-08-15 14:31:56 +0300

    Copy code from hooks/leagueoverseer/index.php into LeagueOverSeerHookController::teamDumpAction()

--- controllers/LeagueOverSeerHookController.php
+++ controllers/LeagueOverSeerHookController.php
@@ -43,7 +43,7 @@ class LeagueOverSeerHookController extends JSONController
         case 'teamNameQuery':
             return $this->forward('teamName');
         case 'teamDump':
-            return 'Dumping team members...';
+            return $this->forward('teamDump');
         default:
             throw new BadRequestException();
         }
@@ -62,5 +62,30 @@ class LeagueOverSeerHookController extends JSONController
             "bzid" => $bzid,
             "team" => ($team->isValid()) ? preg_replace("/&[^\s]*;/", "", $team->getName()) : ''
         ));
+    }
+
+    public function teamDumpAction()
+    {
+        if ($this->version < 1) {
+            throw new BadRequestException();
+        }
+
+        // Create an array to store all teams and the BZIDs
+        $teamArray = array();
+
+        foreach (Team::getTeams() as $team) {
+            $memberList = "";
+
+            foreach ($team->getMembers() as $member) {
+                $memberList .= $member->getBZID() . ",";
+            }
+
+            $teamArray[] = array(
+                "team" => preg_replace("/&[^\s]*;/", "", $team->getName()),
+                "members" => rtrim($memberList, ",")
+            );
+        }
+
+        return new JsonResponse(array("teamDump" => $teamArray));
     }
 }
