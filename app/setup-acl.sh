e4c5a0f9c9cdc4e480cfa95fcb8296cf6f29bad3
Vladimir Jimenez <allejo@users.noreply.github.com>
2015-02-16 21:45:23 -0800

    Just give access to cache and logs folder

--- app/setup-acl.sh
+++ app/setup-acl.sh
@@ -4,7 +4,7 @@ rm -rf app/cache/*
 rm -rf app/logs/*
 
 HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-FOLDERS="app/ web/assets/imgs/avatars/"
+FOLDERS="app/cache app/logs web/assets/imgs/avatars/"
 
 if hash setfacl 2>/dev/null; then
     sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX $FOLDERS
