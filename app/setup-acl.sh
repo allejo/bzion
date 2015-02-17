8a8b8ce5cad21cf12b4f161dd50cc26d0361c05a
allejo <allejo@me.com>
2015-02-16 21:42:44 -0800

    Added script to quickly configure ACL for necessary folders

---
+++ app/setup-acl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf app/cache/*
+rm -rf app/logs/*
+
+HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+FOLDERS="app/ web/assets/imgs/avatars/"
+
+if hash setfacl 2>/dev/null; then
+    sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX $FOLDERS
+    sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX $FOLDERS
+else
+    sudo chmod -R +a "$HTTPDUSER allow delete,write,append,file_inherit,directory_inherit" $FOLDERS
+    sudo chmod -R +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" $FOLDERS
+fi
