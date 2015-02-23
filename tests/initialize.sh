f88e732dd754a8cdbbeaa907d7cca9ad30deff2b
kongr45gpen <electrovesta@gmail.com>
2015-02-23 02:01:04 +0200

    Another try to fix the build

--- tests/initialize.sh
+++ tests/initialize.sh
@@ -10,6 +10,8 @@ php composer.phar install $COMPOSER_ARGS --prefer-source
 FILE=app/config.yml
 cp app/config.example.yml $FILE
 
+sed -i 's/username: bzion_admin/username: root/' $FILE
+sed -i 's/password: password/password: /' $FILE
 sed -i 's/development:\s*false/development: force/' $FILE
 cat << EOF >> $FILE
     testing:
@@ -22,4 +24,4 @@ EOF
 echo "error_reporting (E_ALL | E_STRICT | E_DEPRECATED);" >> bzion-load.php
 
 mysql -e "CREATE DATABASE IF NOT EXISTS bzion;" -uroot;
-php composer.phar migrate-database testing
+php composer.phar migrate-database
