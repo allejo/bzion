18d1dc880763aa4381fad350caba5140628d3cff
kongr45gpen <electrovesta@gmail.com>
2015-03-21 22:33:22 +0200

    Don't cache the profiler twig files, since they are rendered by Symfony's twig instance

--- src/Twig/TwigCacheWarmer.php
+++ src/Twig/TwigCacheWarmer.php
@@ -17,6 +17,15 @@ class TwigCacheWarmer implements CacheWarmerInterface
         foreach ($iterator as $i) {
             $matches = array();
             if (preg_match('/^(' . preg_quote($directory, '/') . '\\/)(.+\.twig)$/i', $i, $matches)) {
+
+                list($topDirectory) = explode("/", $matches[2], 2);
+
+                // Don't cache the profiler files, since they are rendered by a
+                // different twig instance
+                if ($topDirectory === 'Profiler') {
+                    continue;
+                }
+
                 $twig->loadTemplate($matches[2]);
             }
         }
