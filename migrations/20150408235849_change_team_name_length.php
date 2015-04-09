033ab20d5ab332bfd98b93a9a0391f0dd64df316
allejo <allejo@me.com>
2015-04-08 17:11:11 -0700

    Increase team length to the answer to life

---
+++ migrations/20150408235849_change_team_name_length.php
@@ -0,0 +1,41 @@
+<?php
+
+use BZIon\Migration\AbstractMigration;
+
+class ChangeTeamNameLength extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $table = $this->table('teams');
+
+        foreach ($table->getColumns() as $column)
+        {
+            if ($column->getName() === 'name' || $column->getName() === 'alias')
+            {
+                $column->setLimit(42);
+
+                if ($column->getName() === 'name')
+                {
+                    $column->setComment("The team's name");
+                }
+                else
+                {
+                    $column->setComment("The team's URL slug for viewing the team's profile");
+                }
+
+                $table->changeColumn($column->getName(), $column);
+            }
+        }
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
