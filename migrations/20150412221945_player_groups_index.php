5ba1f09c185028cd8aee3c6d0e3d060b5d7a1890
kongr45gpen <electrovesta@gmail.com>
2015-04-13 01:56:30 +0300

    Add a unique index for player-group associations

---
+++ migrations/20150412221945_player_groups_index.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class PlayerGroupsIndex extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     */
+     public function change()
+     {
+         $this->table('player_groups')
+            ->addIndex(array('player', 'group'), array('unique' => true))
+            ->update();
+     }
+}
