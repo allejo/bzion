1879f2a80efd1eb552770e02bc6c1f8b6b8fe4e9
kongr45gpen <electrovesta@gmail.com>
2015-06-26 01:20:37 +0300

    Use select2 for lists in the profile page

--- web/assets/js/profile.js
+++ web/assets/js/profile.js
@@ -1,22 +1,19 @@
-/**
- * Perform an AJAX request to update a player's profile
- */
-function updateProfile() {
+function formatCountry(country) {
+    if (!country.id) {
+        return country.text;
+    }
 
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: baseURLNoHost + "/ajax/updateProfile.php",
-        data: {
-            avatar: $(".profile_avatar").val(),
-            description: $(".profile_description").val(),
-            timezone: $(".profile_timezone").val()
-        }
-    }).done(function(msg) {
-        // Find the notification type
-        type = msg.success ? "success" : "error";
+    var $country = $(
+        '<div class="c-flag c-flag--' + country.element.value.toLowerCase() + '"></div> <span>' + country.text + '</span>'
+    );
+    return $country;
+};
 
-        notify(msg.message, type);
+$(document).ready(function() {
+    $("#form_country").select2({
+        templateResult: formatCountry,
+        templateSelection: formatCountry,
     });
 
-}
+    $("#form_timezone").select2();
+});
