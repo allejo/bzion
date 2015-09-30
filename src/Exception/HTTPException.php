4316e50716aa536b8c669de3e5b31c75093d7150
kongr45gpen <electrovesta@gmail.com>
2015-09-30 20:56:06 +0300

    Fix the build for HHVM

--- src/Exception/HTTPException.php
+++ src/Exception/HTTPException.php
@@ -15,6 +15,33 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 abstract class HTTPException extends Exception implements HttpExceptionInterface
 {
     /**
+     * The exception's message
+     * @var null|string
+     */
+    protected $message = null;
+
+    /**
+     * Construct new Exception
+     * @param null|string $message  The Exception's message
+     *                              (null for the default one)
+     * @param int         $code     The Exception's code
+     * @param Exception   $previous The previous exception
+     */
+    public function __construct(
+        $message = null,
+        $code = 0,
+        Exception $previous = null
+    ) {
+        if ($message === null) {
+            // Workaround for HHVM that doesn't support shadowing of properties
+            // for Exceptions
+            $message = $this->message;
+        }
+
+        return parent::__construct($message, $code, $previous);
+    }
+
+    /**
      * The HTTP error code that the response should include
      * @return int
      */
