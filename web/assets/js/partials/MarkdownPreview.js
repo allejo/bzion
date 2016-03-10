2ff8998194425ec0b60eb81f3cafa2ace6914080
allejo <allejo@me.com>
2016-03-10 00:26:55 -0800

    Added markdown preview for news section
    - Let's add a feature that no one asked for!

---
+++ web/assets/js/partials/MarkdownPreview.js
@@ -0,0 +1,23 @@
+var md = window.markdownit();
+
+function toggleToolbar () {
+    $(".js-mdEditor").toggleClass('active');
+    $(".js-mdPreview").toggleClass('active');
+
+    $("#c-md-editor__canvas").toggleClass("c-md-editor__canvas--editor c-md-editor__canvas--preview");
+}
+
+$(function () {
+    $(".js-mdEditor").click(function () {
+        toggleToolbar();
+    });
+
+    $(".js-mdPreview").click(function() {
+        toggleToolbar();
+
+        var markdown = $("#form_content").val();
+        var result = md.render(markdown);
+
+        $("#c-md-editor__preview").html(result);
+    });
+});
