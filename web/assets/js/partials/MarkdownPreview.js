4d125d66edf321b54776a70176ae2449a66bc1c0
kongr45gpen <electrovesta@gmail.com>
2016-03-13 15:14:30 +0200

    Fix javascript not working on some pages

--- web/assets/js/partials/MarkdownPreview.js
+++ web/assets/js/partials/MarkdownPreview.js
@@ -1,23 +1,25 @@
-var md = window.markdownit();
+if (window.markdownit !== undefined) {
+    var md = window.markdownit();
 
-function toggleToolbar () {
-    $(".js-mdEditor").toggleClass('active');
-    $(".js-mdPreview").toggleClass('active');
+    function toggleToolbar() {
+        $(".js-mdEditor").toggleClass('active');
+        $(".js-mdPreview").toggleClass('active');
 
-    $("#c-md-editor__canvas").toggleClass("c-md-editor__canvas--editor c-md-editor__canvas--preview");
+        $("#c-md-editor__canvas").toggleClass("c-md-editor__canvas--editor c-md-editor__canvas--preview");
+    }
+
+    $(function () {
+        $(".js-mdEditor").click(function () {
+            toggleToolbar();
+        });
+
+        $(".js-mdPreview").click(function () {
+            toggleToolbar();
+
+            var markdown = $("#form_content").val();
+            var result = md.render(markdown);
+
+            $("#c-md-editor__preview").html(result);
+        });
+    });
 }
-
-$(function () {
-    $(".js-mdEditor").click(function () {
-        toggleToolbar();
-    });
-
-    $(".js-mdPreview").click(function() {
-        toggleToolbar();
-
-        var markdown = $("#form_content").val();
-        var result = md.render(markdown);
-
-        $("#c-md-editor__preview").html(result);
-    });
-});
