35d4a44800223f0d8021de416d0c3ae5794a33d9
allejo <allejo@me.com>
2017-06-20 21:03:58 -0700

    Add new JS charting library

---
+++ assets/js/src/charts.js
@@ -0,0 +1,48 @@
+var Chartist = require('chartist-webpack');
+
+function buildWinLossDrawPieCharts() {
+    var $pies = $('div[data-graph="pie-wld"]');
+
+    $pies.each(function () {
+        var $this = $(this);
+
+        new Chartist.Pie(this, {
+            series: [
+                {
+                    value: $this.data('wins'),
+                    name: 'Wins',
+                    className: 'ct-chart-series--wins'
+                },
+                {
+                    value: $this.data('losses'),
+                    name: 'Losses',
+                    className: 'ct-chart-series--losses'
+                },
+                {
+                    value: $this.data('draws'),
+                    name: 'Draws',
+                    className: 'ct-chart-series--draws'
+                }
+            ]
+        });
+    });
+}
+
+module.exports = function() {
+    // Due to hidden DOM objects not having a height and width, if the chart's parent is `display: none`, the chart won't
+    // display. In this case, our charts may reside in tab panels, so we have to trigger a chart update when the tab is
+    // clicked.
+    //
+    // see: https://github.com/gionkunz/chartist-js/issues/119
+
+    $('[role="tab"]').on('shown.bzion.tab', function (event, tab) {
+        var $tab = $(tab);
+
+        $tab.find('.ct-chart').each(function (i, e) {
+            e.__chartist__.update();
+        });
+    });
+
+    // Start building all of the generic charts that we support
+    buildWinLossDrawPieCharts();
+};
