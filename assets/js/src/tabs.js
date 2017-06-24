58589289b3c680c6d51c6b23b676ebd0b614afe0
allejo <allejo@me.com>
2017-06-23 23:42:56 -0700

    Accessibility and responsiveness done for team page

--- assets/js/src/tabs.js
+++ assets/js/src/tabs.js
@@ -1,5 +1,49 @@
+/**
+ * Check whether or not the tabs are visible
+ *
+ * @returns {boolean}
+ */
+function areTabsVisible() {
+    return ($('.c-tabs__header:visible').length >= 1);
+}
+
+/**
+ * Toggle the tabs' ARIA visibility based on whether or the tabs are visible via CSS
+ */
+function toggleTabsAriaVisible() {
+    $('.c-tabs__header').attr('aria-hidden', (areTabsVisible()) ? 'false' : 'true');
+}
+
+/**
+ * Toggle the tab panels' ARIA visibility based on whether or not tabs are visible
+ */
+function toggleAriaTabPanels() {
+    var $activeTab = $('[aria-selected="true"]');
+
+    $('[role="tabpanel"]').each(function () {
+        var $this = $(this);
+
+        // Don't hide the tab panel for the currently selected tab
+        if ($this.attr('id') === $activeTab.attr('aria-controls')) {
+            return;
+        }
+
+        $this.attr('aria-hidden', (areTabsVisible()) ? 'true' : 'false');
+    });
+}
+
+// To support responsive behavior, listen for the window resize event and change the tabs' visibility
+window.addEventListener('resize', function() {
+    toggleTabsAriaVisible();
+    toggleAriaTabPanels();
+});
+
+// Our main tabbing functionality. This should all be based on ARIA attributes
 module.exports = function() {
     var $tab = $('[role="tab"]');
+
+    toggleTabsAriaVisible();
+    toggleAriaTabPanels();
 
     $tab.click(function() {
         var $this = $(this);
