c69c4bac7b3eb6970ee2f001293008a63115d679
allejo <allejo@me.com>
2017-07-21 01:25:55 -0700

    Add misc JS helper fxns

---
+++ assets/js/src/utilities.js
@@ -0,0 +1,17 @@
+module.exports = {
+    getURLParameter: function (sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+
+        return false;
+    }
+};
