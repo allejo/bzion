2d98947d2355172bd19a3df6ad9777709535a6eb
allejo <allejo@me.com>
2017-09-15 18:54:55 -0700

    Redesign UI for notifications

--- controllers/NotificationController.php
+++ controllers/NotificationController.php
@@ -1,58 +1,29 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-
 class NotificationController extends HTMLController
 {
-    public function listAction(Player $me, Request $request)
+    public function listAction(Player $me)
     {
         $this->requireLogin();
 
-        $query = $this->getQueryBuilder()
+        $query = Notification::getQueryBuilder()
             ->where('receiver')->is($me)
             ->limit(15)->fromPage($this->getCurrentPage())
-            ->sortBy('timestamp')->reverse();
+            ->sortBy('timestamp')->reverse()
+        ;
 
         $notifications = $query->getModels($fast = true);
+        $notificationsGrouped = array_group_by($notifications, function ($item) {
+            /** @var Notification $item */
+            return $item->getTimestamp()->format('F Y');
+        });
 
         // Mark the notifications as read after fetching them, so we can show
         // to the user which notifications he hadn't seen
         $query->markRead();
 
-        return array('notifications' => $this->chunk($notifications));
-    }
-
-    /**
-     * Separates notifications based on their timestamp
-     *
-     * @param  Notification[] $notifications The list of notifications
-     * @return array[]
-     */
-    private function chunk($notifications)
-    {
-        $result = array();
-        $index  = -1;
-
-        foreach ($notifications as $notification) {
-            $timezone = $this->getMe()->getTimezone();
-            $date = $notification->getTimestamp()
-                ->copy()
-                ->timezone($timezone)
-                ->startOfMonth();
-
-            // Create a new element in the $result array for every month
-            if ($index == -1 || $result[$index]['date'] != $date) {
-                $result[] = array(
-                    'notifications' => array(),
-                    'date'          => $date
-                );
-
-                ++$index;
-            }
-
-            $result[$index]['notifications'][] = $notification;
-        }
-
-        return $result;
+        return [
+            'notifications' => $notificationsGrouped
+        ];
     }
 }
