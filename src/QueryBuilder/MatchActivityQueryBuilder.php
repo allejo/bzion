a6b45fbd0340057ce02b0084059ba5d1085dad6b
allejo <allejo@me.com>
2017-10-02 14:27:11 -0700

    Move shared activity calculations to dedicated class

---
+++ src/QueryBuilder/MatchActivityQueryBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @property BaseModel $type
+ */
+class MatchActivityQueryBuilder extends QueryBuilder
+{
+    protected function includeMatchActivity($selectColumns, $leftJoinOn)
+    {
+        $type = $this->type;
+        $columns = $type::getEagerColumns($this->getFromAlias());
+
+        $this->columns['activity'] = 'activity';
+        $this->extraColumns = 'SUM(m2.activity) AS activity';
+        $this->extras .= '
+          LEFT JOIN
+            (SELECT
+              m.id,'
+              . implode(',', $selectColumns) . ',
+              TIMESTAMPDIFF(SECOND, timestamp, NOW()) / 86400 AS days_passed,
+              (0.0116687059537612 * (POW((45 - LEAST((SELECT days_passed), 45)), (1/6)) + ATAN(31 - (SELECT days_passed)) / 2)) AS activity
+            FROM
+              matches m
+            WHERE
+              DATEDIFF(NOW(), timestamp) <= 45
+            ORDER BY
+              timestamp DESC) m2 ON ' . $leftJoinOn
+        ;
+
+        $this->groupQuery = 'GROUP BY ' . $columns;
+
+        return $this;
+    }
+}
--- src/QueryBuilder/TeamQueryBuilder.php
+++ src/QueryBuilder/TeamQueryBuilder.php
@@ -1,32 +1,12 @@
 <?php
 
-class TeamQueryBuilder extends QueryBuilder
+class TeamQueryBuilder extends MatchActivityQueryBuilder
 {
     public function withMatchActivity()
     {
-        /** @var BaseModel $type */
-        $type = $this->type;
-        $columns = $type::getEagerColumns($this->getFromAlias());
-
-        $this->columns['activity'] = 'activity';
-        $this->extraColumns = 'SUM(m2.activity) AS activity';
-        $this->extras .= '
-          LEFT JOIN
-            (SELECT
-              m.id,
-              m.team_a,
-              m.team_b,
-              TIMESTAMPDIFF(SECOND, timestamp, NOW()) / 86400 AS days_passed,
-              (0.0116687059537612 * (POW((45 - LEAST((SELECT days_passed), 45)), (1/6)) + ATAN(31 - (SELECT days_passed)) / 2)) AS activity
-            FROM
-              matches m
-            WHERE
-              DATEDIFF(NOW(), timestamp) <= 45 AND m.match_type = "official"
-            ORDER BY
-              timestamp DESC) m2 ON teams.id = m2.team_a OR teams.id = m2.team_b
-        ';
-        $this->groupQuery = 'GROUP BY ' . $columns;
-
-        return $this;
+        return $this->includeMatchActivity(
+            ['m.team_a', 'm.team_b'],
+            'teams.id = m2.team_a OR teams.id = m2.team_b'
+        );
     }
 }
